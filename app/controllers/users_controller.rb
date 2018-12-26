a5dde28c32133125ff0d2af8a6d5ef4c1f3003fb
Leonardo Tegon <ltegon93@gmail.com>
2018-12-26 13:52:08 -0200

    Include `api_token` as a default strategy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    warden.authenticate!(:api_token)
     render json: current_user.to_json
   end
 end
